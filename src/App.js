fb5c58168b703b9e4c09efc0327035a553694d24
Aharnish KS <aharnishks@gmail.com>
2022-06-29 13:13:31 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
+import LoginPage from './components/LoginPage';
+import Todos from './components/Todos';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  return ( 
+    <div className="App">
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={ <Todos />} />
+    <Route path="/login" element={<LoginPage />} />
+    </Routes>
+    </BrowserRouter>
+    </div>
+   );
 }
-
+ 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import './components.css'
+
+const Todos = () => {
+    const [user, setUser] = useState('');
+    const [loading, setLoading] = useState(true)
+
+    
+    const [todos, setTodos] = useState([ 
+        {user_id: "Aharnish", todo: "Complete this project", status: false}
+    ]);
+
+    useEffect(() => {
+        document.title = "Todo List"
+
+    }, [todos])
+    if (loading) {
+        return (
+            <div className="todos">
+        <div className="container">
+            <div class="row  justify-content-center">
+                    <div class="col-lg-6">
+
+                        <div class="list-group m-3 p-3" >
+                            
+                        { todos.map(todo => {
+                            return (
+                                <div class="list-group-item py-3 border-white bg-dark text-blue">
+                                    <div className="row">
+                                        <div class="col-10 done">
+                                            <h5 class="mb-1">{todo.todo}</h5> 
+                                        </div>
+                                        <div class="col-2">
+                                            <span className="float-end h5">
+                                            <i class="fa-solid fa-xmark"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        }) }
+
+
+                        </div>
+
+                    </div>
+                </div>
+        </div>
+        </div>
+    )
+    }
+    else {
+        <div class="text-center">
+          <br />
+          <br />
+          <br />
+          <h1 class="fw-bold">Loading.....</h1>
+        </div>
+      }
+}
+ 
+export default Todos;

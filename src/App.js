a29ebe13d41385a0684bd1f9c82551f1824a8b8b
Adensty7 <78910585+Adensty7@users.noreply.github.com>
2022-06-30 19:29:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import LoginPage from './components/LoginPage';
 import TodosPage from './components/TodosPage';
+import Cookies from 'universal-cookie';
 
 const App = () => {
+  const cookies = new Cookies();
+
+  const UserID = cookies.get('UserID');
+
   return ( 
     <div className="App">
     <BrowserRouter>
     <Routes>
-    <Route exact path='/' element={ <TodosPage /> } />
-    <Route path="/login" element={<LoginPage />} />
+    { UserID && <Route exact path='/' element={ <TodosPage /> } />}
+    { !UserID && <Route path="/login" element={<LoginPage />} /> }
     </Routes>
     </BrowserRouter>
     </div>
